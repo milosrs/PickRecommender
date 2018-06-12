f62c8724f27ed3d3e5e3ec5ea773260b656057ac
milosrs <milosribar@yahoo.com>
2018-06-12 18:10:18 +0200

    Malo fronta malo diskoteka

--- front/PickRecommender/src/app/components/champion-picks/champion-picks.component.ts
+++ front/PickRecommender/src/app/components/champion-picks/champion-picks.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ChampionService } from '../../services/champion.service';
 import { ChampionViewList } from '../../model/champion-view-list';
 import { HelperFunctions } from '../../shared/util/helper-functions';
+import { AuthService } from '../../services/auth.service';
+import { Token } from '../../model/token';
 
 @Component({
   selector: 'champion-picks',
@@ -11,11 +13,16 @@ import { HelperFunctions } from '../../shared/util/helper-functions';
 export class ChampionPicksComponent implements OnInit {
   
   private champList: ChampionViewList;
+  private token: Token;
 
-  constructor(protected championService: ChampionService) { }
+  constructor(protected championService: ChampionService, protected auth: AuthService) { }
 
   ngOnInit() {
     this.championService.getChampViewList().subscribe(item => this.champList = item);
+    this.token = this.auth.getToken();
   }
 
+  isUserLoggedIn() {
+    return this.auth.isLoggedInSimple();
+  }
 }
